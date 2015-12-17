a9a1ca156ae34c34982606611a288b11b0a6ad17
Tiernan OToole <lotas_sm@hotmail.com>
2015-12-17 13:57:52 +0000

    cleaning code.
    Related Work Items: #19

--- B2Uploader/Program.cs
+++ B2Uploader/Program.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using B2Classes;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -146,73 +147,5 @@ namespace B2Uploader
         }
     }
 
-    public class AuthorizeResponse
-    {        
-        public string accountId { get; set; }
-        public string apiUrl { get; set; }
-        public string authorizationToken { get; set; }
-        public string downloadUrl { get; set; }   
-    }
-
-    public class CreateBucketResponse
-    {
-        public string bucketId { get; set; }
-        public string accountId { get; set; }
-        public string bucketName { get; set; }
-        public string bucketType { get; set; }
-    }
-
-    public class CreateBucketRequest
-    {
-        public string accountId { get; set; }
-        public string bucketName { get; set; }
-        public string bucketType { get; set; }
-    }
-
-    public class GetUploadURLRequest
-    {
-        public string bucketId { get; set; }
-    }
-
-    public class GetUploadURLResponse
-    {
-        public string bucketId { get; set; }
-        public string uploadUrl { get; set; }
-        public string authorizationToken { get; set; }
-    }
-
-    public class ListBucketsRequest
-    {
-        public string accountId { get; set; }
-    }
-
-    public class ListBucketsResponse
-    {
-        public List<Bucket> buckets { get; set; }
-    }
-
-    public class Bucket
-    {
-        public string bucketId { get; set; }
-        public string accountId { get; set; }
-        public string bucketName { get; set; }
-        public string bucketType { get; set; }
-    }
-
-    public class FileInfo
-    {
-        public string author { get; set; }
-    }
-
-    public class UploadFileResponse
-    {
-        public string fileId { get; set; }
-        public string fileName { get; set; }
-        public string accountId { get; set; }
-        public string bucketId { get; set; }
-        public int contentLength { get; set; }
-        public string contentSha1 { get; set; }
-        public string contentType { get; set; }
-        public FileInfo fileInfo { get; set; }
-    }
+    
 }
