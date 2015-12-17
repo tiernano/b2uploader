18c8183d97990d88ea3d1d10b4cd100c27910561
Tiernan OToole <tiernan@tiernanotoole.ie>
2015-12-17 21:57:14 +0000

    bit more cleaning up and tweaks to multithreading
    Related Work Items: #21, #23, #26

--- B2Uploader/Program.cs
+++ B2Uploader/Program.cs
@@ -20,9 +20,9 @@ namespace B2Uploader
         [Option('a', "appkey", HelpText = "Application Key", Required=true)]
         public string ApplicationKey { get; set; }
 
-        [Option('d', "directory", HelpText="Directory you want to upload", Required=true)]
-        public string Directory {get;set;}
-
+        [Option('d', "directory", HelpText = "Directory you want to upload", Required = true)]
+        public string Directory { get; set; }
+        
         [Option('v', "verbose", HelpText="Verbose Output")]
         public bool Verbose{get;set;}
     }
@@ -51,7 +51,7 @@ namespace B2Uploader
                 {
                     //check if file already exists
 
-                    string fileName = s.Replace('\\', '_');
+                    string fileName = s.Replace('\\', '/');
 
                     var existingFiles = ListFileNames(new ListFileNamesRequest() { bucketId = bucket.bucketId, startFileName = fileName }, auth.apiUrl, auth.authorizationToken);
                     bool found = false;
@@ -61,6 +61,7 @@ namespace B2Uploader
                         {
                             //check the file size
                             System.IO.FileInfo fi = new System.IO.FileInfo(s);
+                            
                             if (fi.Length == x.size)
                             {
                                 found = true;
@@ -92,7 +93,7 @@ namespace B2Uploader
         }
 
         static AuthorizeResponse AuthorizeUser(string accountId, string applicationKey)
-        {            
+        {
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("https://api.backblaze.com/b2api/v1/b2_authorize_account");
             string credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes(accountId + ":" + applicationKey));
             webRequest.Headers.Add("Authorization", "Basic " + credentials);
@@ -103,44 +104,29 @@ namespace B2Uploader
             return JsonConvert.DeserializeObject<AuthorizeResponse>(responseString);
         }
 
-        static ListBucketsResponse ListBuckets(ListBucketsRequest request, string authToken, string apiURL)
-        {            
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(apiURL + "/b2api/v1/b2_list_buckets");
-            string body = JsonConvert.SerializeObject(request);
-            var data = Encoding.UTF8.GetBytes(body);
-            webRequest.Method = "POST";
-            webRequest.Headers.Add("Authorization", authToken);
-            webRequest.ContentType = "application/json; charset=utf-8";
-            webRequest.ContentLength = data.Length;
-            using (var stream = webRequest.GetRequestStream())
-            {
-                stream.Write(data, 0, data.Length);
-                stream.Close();
-            }
-            WebResponse response = (HttpWebResponse)webRequest.GetResponse();
-            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-            response.Close();
+        static ListBucketsResponse ListBuckets(ListBucketsRequest request, string authToken, string apiUrl)
+        {
+            var headers = GetAuthHeaders(authToken);
+
+            string responseString = MakeWebRequest(apiUrl + "/b2api/v1/b2_list_buckets", headers, request);
+
 
             return JsonConvert.DeserializeObject<ListBucketsResponse>(responseString);
         }
 
+        static List<Tuple<string,string>> GetAuthHeaders(string authToken)
+        {
+            List<Tuple<string, string>> headers = new List<Tuple<string, string>>();
+            headers.Add(new Tuple<string, string>("Authorization", authToken));
+            return headers;
+        }
+
         static GetUploadURLResponse GetUploadURL(GetUploadURLRequest request, string apiUrl, string authToken)
-        {          
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(apiUrl + "/b2api/v1/b2_get_upload_url");
-            string body = JsonConvert.SerializeObject(request);
-            var data = Encoding.UTF8.GetBytes(body);
-            webRequest.Method = "POST";
-            webRequest.Headers.Add("Authorization", authToken);
-            webRequest.ContentType = "application/json; charset=utf-8";
-            webRequest.ContentLength = data.Length;
-            using (var stream = webRequest.GetRequestStream())
-            {
-                stream.Write(data, 0, data.Length);
-                stream.Close();
-            }
-            WebResponse response = (HttpWebResponse)webRequest.GetResponse();
-            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-            response.Close();
+        {
+
+            var headers = GetAuthHeaders(authToken); 
+            string responseString = MakeWebRequest(apiUrl + "/b2api/v1/b2_get_upload_url", headers, request);
+            
             return JsonConvert.DeserializeObject<GetUploadURLResponse>(responseString);
         }
 
@@ -148,23 +134,15 @@ namespace B2Uploader
         {
 
             byte[] bytes = System.IO.File.ReadAllBytes(filePath);
-
             String sha1 = GetSha1(bytes);
-            
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(uploadUrl);
-            webRequest.Method = "POST";
-            webRequest.Headers.Add("Authorization", authToken);
-            webRequest.Headers.Add("X-Bz-File-Name", filePath.Replace('\\','_'));
-            webRequest.Headers.Add("X-Bz-Content-Sha1", sha1);
-            webRequest.ContentType = contentType;
-            using (var stream = webRequest.GetRequestStream())
-            {
-                stream.Write(bytes, 0, bytes.Length);
-                stream.Close();
-            }
-            WebResponse response = (HttpWebResponse)webRequest.GetResponse();
-            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-            response.Close();
+
+            var headers = GetAuthHeaders(authToken);
+
+            headers.Add(new Tuple<string, string>("X-Bz-File-Name", filePath.Replace('\\', '/')));
+            headers.Add(new Tuple<string, string>("X-Bz-Content-Sha1", sha1));
+
+            string responseString = MakeWebRequest(uploadUrl, headers, bytes, contentType);
+
             var resp = JsonConvert.DeserializeObject<UploadFileResponse>(responseString);
 
             if(resp.contentSha1 == sha1)
@@ -181,23 +159,49 @@ namespace B2Uploader
 
         static ListFileNamesResponse ListFileNames(ListFileNamesRequest request, string apiUrl, string authToken)
         {
+            var headers = GetAuthHeaders(authToken);
+            string responseString =  MakeWebRequest(apiUrl + "/b2api/v1/b2_list_file_names", headers, request);
+
+            return JsonConvert.DeserializeObject<ListFileNamesResponse>(responseString);
+        }
+
+        static string MakeWebRequest<T>(string url, List<Tuple<string,string>> headers, T item, string contentType = "application/json; charset=utf-8")
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            byte[] data;
+
+            if (typeof(T) == typeof(byte[]))
+            {
+                data = (byte[])(object) item;
+            }
+            else {
+                string body = JsonConvert.SerializeObject(item);
+
+                data = Encoding.UTF8.GetBytes(body);
+            }
             
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(apiUrl + "/b2api/v1/b2_list_file_names");
-            string body = JsonConvert.SerializeObject(request);
-            var data = Encoding.UTF8.GetBytes(body);
-            webRequest.Method = "POST";
-            webRequest.Headers.Add("Authorization", authToken);
-            webRequest.ContentType = "application/json; charset=utf-8";
-            webRequest.ContentLength = data.Length;
-            using (var stream = webRequest.GetRequestStream())
+            req.Method = "POST";
+
+            foreach(var head in headers)
+            {
+                req.Headers.Add(head.Item1, head.Item2);
+            }
+
+            req.ContentType = contentType;
+            req.ContentLength = data.Length;
+            using (var stream = req.GetRequestStream())
             {
                 stream.Write(data, 0, data.Length);
                 stream.Close();
             }
-            WebResponse response = (HttpWebResponse)webRequest.GetResponse();
+            WebResponse response = (HttpWebResponse)req.GetResponse();
             var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
             response.Close();
-            return JsonConvert.DeserializeObject<ListFileNamesResponse>(responseString);
+
+            return responseString;
+
+
+
         }
 
         private static string GetSha1(byte[] bytes)
