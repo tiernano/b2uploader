25db86508ed64a502f17317d002add8b66fd0c9e
Tiernan OToole <tiernan@tiernanotoole.ie>
2015-12-19 00:11:44 +0000

    tweaking build scripts

--- build.cmd
+++
@@ -1,1 +0,0 @@
-%SYSTEMROOT%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe "%~dp0\B2Uploader.sln" /p:OutputPath="%~dp0\bin"
---
+++ buildx86.cmd
@@ -0,0 +1,1 @@
+%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "%~dp0\B2Uploader.sln" /p:OutputPath="%~dp0\bin"
