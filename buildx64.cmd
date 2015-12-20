70314c57ef1bc665524830e15feef95150647ab3
Tiernan OToole <tiernan@tiernanotoole.ie>
2015-12-20 15:08:23 +0000

    new build versions (proper x64 and x86 now!)

--- buildx64.cmd
+++ buildx64.cmd
@@ -1,1 +1,1 @@
-%SYSTEMROOT%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe "%~dp0\B2Uploader.sln" /p:OutputPath="%~dp0\bin\64"
+%SYSTEMROOT%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe /p:Configuration=Releasex64,OutputPath="%~dp0\bin\64"
--- buildx86.cmd
+++ buildx86.cmd
@@ -1,1 +1,1 @@
-%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "%~dp0\B2Uploader.sln" /p:OutputPath="%~dp0\bin\86"
+%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "%~dp0\B2Uploader.sln" /p:Configuration=Releasex86,OutputPath="%~dp0\bin\64"
